f631f302f70ae8bc2dd029b2fc81e535d694f231
johnny <enriquemartin1402@gmail.com>
2025-08-17 16:57:52 +0200

    Change nvim options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,16 +1,41 @@
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+
+-- Line settings
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.opt.wrap = false
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.autoindent = true
-vim.opt.smartcase = true
 vim.opt.cursorline = true
+vim.opt.wrap = false
+
+-- UI settings
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.opt.signcolumn = "yes"
+vim.opt.showmode = false
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Split window settings
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Scroll page settings
+vim.opt.scrolloff = 5
+
+-- Search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Undo persistance
 vim.opt.undofile = true
