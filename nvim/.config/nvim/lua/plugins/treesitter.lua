0734da35359b44eacf187c685510f7a9338ea8a7
johnny <enriquemartin1402@gmail.com>
2025-08-17 15:59:16 +0200

    Add tresitter support for more languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,39 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
-      ensure_installed = {
-        "lua",
-        "rust",
-        "typescript",
-        "javascript",
-        "python",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
-
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"lua",
+					"rust",
+					"typescript",
+					"javascript",
+					"python",
+					"zig",
+					"c",
+					"bash",
+					"nu",
+					"diff",
+					"html",
+					"json",
+					"jsdoc",
+					"jsonc",
+					"markdown",
+					"markdown_inline",
+					"query",
+					"regex",
+					"toml",
+					"tsx",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
