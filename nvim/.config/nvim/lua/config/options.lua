2b1f6cdf4714c68df5d737caa45c9978087a9a35
johnny <enriquemartin1402@gmail.com>
2025-08-19 11:01:02 +0200

    Change nvim tab spaces

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Indentation
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.smarttab = true
 vim.opt.smartindent = true
